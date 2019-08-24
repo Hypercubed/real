5d3503b282ae75e4ba69343e4e8dc558a05f1269
Jayson Harshbarger <hypercubed@gmail.com>
2019-08-24 16:24:20 -0600

    pow

---
+++ test/irrational/pow.test.ts
@@ -0,0 +1,17 @@
+import test from 'ava';
+import Irrational from '../../src/irrational';
+
+test('pow', t => {
+  const pow = (x: any, y: any) => new Irrational(x).pow(new Irrational(y));
+
+  t.is(pow(1, 1).valueOf(), 1);
+  t.is(pow(1, -1).valueOf(), 1);
+
+  // t.is(pow(2, 2).valueOf(), 4);
+  // t.is(pow(4, 0.5).valueOf(), 2);
+
+  /* t.is(pow(0.1, 0.2).toString(), '2e-2');
+  t.is(pow(10, 0.2).toString(), '2.0');
+  t.is(pow(9007199254740992n, 2).toString(), '1.8014398509481984e+16');
+  t.is(pow(10_000_000_000_000_000n, '0.00_000_000_000_000_01').toString(), '1.0000000000000000'); */
+});
