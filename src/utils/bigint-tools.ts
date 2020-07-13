84774d999e5ed44e2ae504539d6f528e3d6cf48b
Hypercubed <hypercubed@gmail.com>
2020-07-13 17:38:38 -0600

    missing utils

---
+++ src/utils/bigint-tools.ts
@@ -0,0 +1,61 @@
+export function bIDiv(dividend: bigint, divisor: bigint, n: number): [bigint, bigint, number] {
+  const sgnDividend = dividend < 0n ? -1n : 1n;
+  const sgnDivisor = divisor < 0n ? -1n : 1n;
+
+  dividend *= sgnDividend;
+  divisor *= sgnDivisor;
+
+  let adj = 0;
+  let result = 0n;
+
+  while (dividend < divisor) {
+    dividend *= 10n;
+    adj++;
+  }
+
+  const S = 10n**BigInt(n);
+
+  for (;;) {
+    result += dividend / divisor;
+    dividend %= divisor;
+
+    if (dividend === 0n || result > S) {
+      break;
+    }
+
+    result *= 10n;
+    dividend *= 10n;
+    adj++;
+  }
+
+  return [result * sgnDividend * sgnDivisor, dividend, adj];
+}
+
+export function bSqr(s: bigint, e: number): [bigint, number] {
+  return [s**2n, e*2];
+}
+
+export function bAbs(s: bigint): bigint {
+  return s < 0n ? -s : s;
+}
+
+// TODO: return remainder
+export function bSqrt(value: bigint): bigint {
+  if (value < 0n) {
+      throw 'square root of negative numbers is not supported'
+  }
+
+  if (value < 2n) {
+      return value;
+  }
+
+  function newtonIteration(n: bigint, x0: bigint): bigint {
+      const x1 = ((n / x0) + x0) >> 1n;
+      if (x0 === x1 || x0 === (x1 - 1n)) {
+          return x0;
+      }
+      return newtonIteration(n, x1);
+  }
+
+  return newtonIteration(value, 1n);
+}
---
+++ src/utils/util.ts
@@ -0,0 +1,65 @@
+
+export type ParseValueInput = bigint | number | string | null | undefined;
+
+// TODO: implement prefix #e - exact, #i - inexact??
+export function parseValue(value: ParseValueInput): [bigint, number, number] {
+  if (value === null || value === undefined) {
+    return [0n, 0, Infinity];
+  }
+
+  if (typeof value === 'number') {
+    value = value.toString();
+    const [s, e, p] = parseValue(value);
+    const isInteger = value.indexOf('.') < 0 && value.search(/[eE]/) < 0;
+    // Integers are accurate up to 15 digits.
+    // The maximum number of decimals is 17
+    return [s, e, isInteger ? 15 : 17];
+  }
+
+  // tslint:disable-next-line
+  if (typeof value === 'bigint') {
+    value = value.toString();
+  }
+
+  value = value.replace(/[_\s]/g, '');  // replace "whitespace"
+
+  const [coefficient, exponent = '0'] = value.split(/[eE]/);
+  let [ip = '0', fp = ''] = coefficient.split('.');
+  const significand = ip + fp;
+  const k = ip.length;
+
+  const s = BigInt(significand);
+  const sign = significand.startsWith('-') ? 1 : 0;
+  const e = parseInt(exponent, 10);
+
+  // Integers (without a period or exponent notation) have infinite precision
+  const isInteger = coefficient.indexOf('.') < 0 && value.search(/[eE]/) < 0;
+
+  // zero
+  if (s === 0n) {
+    const p = isInteger ? Infinity : significand.length - sign;
+    return [s, e, p];
+  }
+
+  const p = isInteger ? Infinity : s.toString().length - sign;
+
+  return [s, e + (k < 0 ? 0 : k - significand.length), p];
+}
+
+export function absDiff(a: bigint, b: bigint): bigint {
+  return a > b ? a - b : b - a;
+}
+
+export function zeroPadRight(s: string, digits: number) {
+  const n = Math.max(0, digits - s.length);
+  return (s + '0'.repeat(n)).slice(0, digits);
+}
+
+export function zeroPadLeft(s: string, digits: number) {
+  const n = Math.max(0, -digits - s.length);
+  return ('0'.repeat(n) + s).slice(digits);
+}
+
+export function gcd(a: bigint, b: bigint): bigint {
+  return b ? gcd(b, a % b) : (a > 0n ? a : -a);
+}
